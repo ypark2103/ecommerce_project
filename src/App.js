298e3711f26890577e01c9eb24675cc900ca9d9c
Youngsoon Park <rpark2103@gmail.com>
2021-07-15 22:05:58 -0700

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <HomePage/>
     </div>
 
   );
